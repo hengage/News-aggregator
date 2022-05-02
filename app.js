c19e534cc23929ca0b002b6778c5237dba2c1fdc
hengage <tunecarterkillz@gmail.com>
2022-05-02 18:31:08 +0100

    feature: made get request for top headline news

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+const { default: axios } = require('axios');
+
 const express = require('express'),
-    app = express()
-    router = express.Router()
-    axios = require('axios'),
-    fs = require('fs')
+    app = express(),
+    router = express.Router(),
+    // axios = require('axios'),
+    fs = require('fs');
 
 require('dotenv').config();
 
 app.get('/', async (req, res, next) => {
-    res.send('<h1>Hello World!</h1>')
+    const url = `https://newsapi.org/v2/top-headlines?country=de&language=de&category=sports&apiKey=${process.env.ACCESS_KEY}`
+
+    try {
+        const news = await axios.get(url);
+        // res.send(news.data);
+        res.json({
+            statusCode: 200,
+            message: "News recieved succesfully!",
+            data: news.data
+        })
+    } catch(error) {
+        if(error.response) {
+            res.json({
+                status:"error",
+                code: "apiKeyInvalid",
+                message: error
+            })
+            console.log(error)
+        }
+    }
+    
+
 })
 
 app.set('port', process.env.PORT)
